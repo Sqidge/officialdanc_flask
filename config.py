65d9dc5deee96d64722f88dd9fb2c0091402b100
Connor Clarkson <connor.sqidge@hotmail.co.uk>
2020-10-24 13:18:04 +0100

    Changed how AWS keys are added

--- config.py
+++ config.py
@@ -6,8 +6,6 @@ from boto3.session import Session
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.flaskenv'),verbose=True)
-with open(os.path.join(basedir, 'app/static/KEYS/aws.json')) as f:
-    key = json.loads(f.read())
 
 
 class Config(object):
@@ -18,9 +16,9 @@ class Config(object):
     EDITS_PREVIEW = False
     EDITS_SUMMERNOTE = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    AWS_ACCESS_KEY_ID = key['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = key['AWS_SECRET_ACCESS_KEY']
-    AWS_DEFAULT_REGION = key['AWS_DEFAULT_REGION']
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION')
     boto_sess = Session(
         region_name=AWS_DEFAULT_REGION,
         aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -30,11 +28,10 @@ class Config(object):
     DYNAMO_CLIENT = boto3.resource('dynamodb', region_name=AWS_DEFAULT_REGION,
                                    aws_access_key_id=AWS_ACCESS_KEY_ID,
                                    aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-    DYNAMO_TABLE = DYNAMO_CLIENT.Table(key['DYNAMO_TABLE'])
-    COGNITO_REGION = key['COGNITO_REGION']
-    COGNITO_USERPOOL_ID = key['COGNITO_USERPOOL_ID']
-    AWS_COGNITO_DOMAIN = key['AWS_COGNITO_DOMAIN']
-    AWS_COGNITO_USER_POOL_ID = key['AWS_COGNITO_USER_POOL_ID']
-    AWS_COGNITO_USER_POOL_CLIENT_ID = key['AWS_COGNITO_USER_POOL_CLIENT_ID']
-    AWS_COGNITO_USER_POOL_CLIENT_SECRET = key['AWS_COGNITO_USER_POOL_CLIENT_SECRET']
-    AWS_COGNITO_REDIRECT_URL = key['AWS_COGNITO_REDIRECT_URL']
+    COGNITO_REGION = os.environ.get('COGNITO_REGION')
+    COGNITO_USERPOOL_ID = os.environ.get('COGNITO_USERPOOL_ID')
+    AWS_COGNITO_DOMAIN = os.environ.get('AWS_COGNITO_DOMAIN')
+    AWS_COGNITO_USER_POOL_ID = os.environ.get('AWS_COGNITO_USER_POOL_ID')
+    AWS_COGNITO_USER_POOL_CLIENT_ID = os.environ.get('AWS_COGNITO_USER_POOL_CLIENT_ID')
+    AWS_COGNITO_USER_POOL_CLIENT_SECRET = os.environ.get('AWS_COGNITO_USER_POOL_CLIENT_SECRET')
+    AWS_COGNITO_REDIRECT_URL = os.environ.get('AWS_COGNITO_REDIRECT_URL')
